1174cea96b8eb8ffdd89b070b321b204a8099ef1
anders-kolstad <anders.kolstad@ntnu.no>
2020-09-22 20:57:05 +0200

    add map and popuptable

--- shiny/app.R
+++ shiny/app.R
@@ -1,20 +1,18 @@
 library(shiny)
 library(shinydashboard)
-library(shinyWidgets)
-library(rdrop2)
-library(stringr)
+#library(shinyWidgets)
 library(DT)
-library(httr)
+#library(httr)
 library(readr)
+library(leaflet)
+library(mapview)
+
+MyTab <-  read_delim("AllCodedDataW_forEviAtlas.csv", 
+                                        ";", escape_double = FALSE, trim_ws = TRUE)
 
 
-MyTab <- read_delim("AllCodedData.txt", 
-                           ";", escape_double = FALSE, trim_ws = TRUE)
 
- 
-
-
-ui <- dashboardPage(title = "Systematic Map",
+ui <- dashboardPage(
                     
           dashboardHeader(title = "Systematic Map",
                                     titleWidth = 400),
@@ -26,6 +24,9 @@ ui <- dashboardPage(title = "Systematic Map",
                 
             column(width=12,
                              
+                    box(width = NULL,
+                        leafletOutput('theMap')),
+                   
                     DTOutput('filtered')
                              
                              
@@ -35,9 +36,37 @@ ui <- dashboardPage(title = "Systematic Map",
 
 
 server <- function(input, output, session) {
+  
   output$filtered <- renderDT(
-    MyTab, selection = 'single'
-  )
+    MyTab, selection = 'single', options = list(scrollX = TRUE))
+  
+  output$theMap <- renderLeaflet({
+      dat <- MyTab[ , ]  # add subsetts here. Shorten author list and title.
+      dat2 <- sp::SpatialPointsDataFrame(coords = dat[,c("coordinates_E","coordinates_N")], 
+                     data = dat,
+                     proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+      
+      theMap <- mapview::mapview(dat2,
+                                 layer.name = "Evidence Point",
+                                 map.types = c("Esri.WorldShadedRelief",
+                                               "Esri.WorldImagery"),
+                                 cex = 5, lwd = 0,
+                                 alpha.regions = 0.5,
+                                 col.regions = "blue",
+                                 popup = leafpop::popupTable(dat2, 
+                                                    row.numbers = F,
+                                                    zcol = c("author_list",
+                                                                   "year",
+                                                                   "journal"))
+                                 
+                                 #label = dat2$title
+                                 )
+                               
+      theMap@map
+      
+    })
+    
+    
   
  
   
