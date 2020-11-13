ecb823b62162bc54689ab4c48fa0a606ad12a4c3
elinakaa <48311339+elinakaa@users.noreply.github.com>
2020-11-13 16:59:16 +0200

    Update consistency_analysis.R

--- consistency_analysis.R
+++ consistency_analysis.R
@@ -1,4 +1,9 @@
-#Consistency analysis: how consistently two reviewers coded the same paper?
+############################
+# Consistency analysis: how consistently did two reviewers code the same paper?
+# Elina Kaarlejärvi
+# 13 Nov 2020
+#############
+
 
 #Script to import coded data from excel spreadsheet.
 #Data taken from Eeva's UiT Box account and read in using a direct download link
@@ -7,7 +12,6 @@
 library(tidyverse)
 library(readxl)#Read in xls from box
 library(irr) #Cohen's kappa and other stats
-
 
 #Data import
 --------------------------------------------------------
@@ -25,113 +29,193 @@ head(scored)
 
 
 #Data cleaning and wrangling
-scored <- scored %>%
+#-----------------------------
+#remove duplicates
+scored_unique <- scored %>% filter(current_score != "remove_duplicate") #2902 records
+  
+scored_unique <- scored_unique %>%
   mutate_at(c('score_1', 'score_2', 'score_3', 
               'person_1', 'person_2', 'person_3', 'current_score'), as.factor)
-str(scored2)
-levels(scored$score_1)
-levels(scored$score_2)
-levels(scored$score_3)
-levels(scored$person_1)
-levels(scored$person_2)
-levels(scored$person_3)
-levels(scored$current_score)
+str(scored_unique)
+levels(scored_unique$score_1)
+levels(scored_unique$score_2)
+levels(scored_unique$score_3)
+levels(scored_unique$person_1)
+levels(scored_unique$person_2)
+levels(scored_unique$person_3)
+levels(scored_unique$current_score)
 
 #check how many scored according to instructions
-scored %>% group_by(score_1) %>% summarize(n()) %>% print(n=30) #14 exclude, based on nothing
-scored %>% group_by(score_2) %>% summarize(n()) %>% print(n=30) # inconsistencies in writing style -> fix
-scored %>% group_by(score_3) %>% summarize(n()) %>% print(n=30) #FINE!
+scored_unique %>% group_by(score_1) %>% summarize(n()) %>% print(n=30) #14 cases with 'exclude' can be skipped, as only 1 reviewer
+scored_unique %>% group_by(score_2) %>% summarize(n()) %>% print(n=30) # inconsistencies in writing style -> fix (below)
+scored_unique %>% group_by(score_3) %>% summarize(n()) %>% print(n=30) #FINE!
 
 #fix inconsistencies in writing style in score_2
-scored2 <- scored %>% 
+# some weird values cannot be classified to any preferred values, e.g. 'cant find'
+scored2 <- scored_unique %>% 
   mutate(score_2 = fct_recode(score_2,
-                              "exclude_abstract" = "exclude_abstact",
-                              "exclude_abstract" = "Exclude_abstract",
-                              "exclude_abstract" = "exclude abstract",
-                              "include" = "Include",
-                              "include" = "Include?",   #1 unsure inclusion, here converted to sure
-                              "include" = "include?"))  #71 unsure inclusions, here converted to sure
-
-#If these unsure inclusions by reviewer 1 are converted to 'exclude_abstract' agreement between reviewers 1 and 2 becomes bit weaker, but no significant changes.
-
+                              'exclude_abstract' = "exclude_abstact",
+                              'exclude_abstract' = "Exclude_abstract",
+                              'include' = "Include",
+                              'include' = "Include?",   #1 unsure inclusion, here converted to include
+                              'include' = "include?"))  #71 unsure inclusions, here converted to sure
 
 # select papers, which were scored as advised
 good_scores <- c("exclude_title", "exclude_abstract", "include")
 
-# papers scored correctly by reviewers 1 AND 2
+# papers scored correctly by only by reviewers 1 AND 2
 ok_scored12 <- scored2 %>%
-  filter(score_1 %in% good_scores)%>%          #3523 records scored correctly by reviewer 1
-  filter(score_2 %in% good_scores)             #3174 records scored correctly by both 1 AND 2
+  filter(score_1 %in% good_scores &
+           score_2 %in% good_scores &
+           !(score_3 %in% good_scores))%>%
+  select(score_1, score_2, current_score, ID_Eeva)#n = 2597 
 
+# papers scored correctly by only by reviewers 1 AND 3
 ok_scored13 <- scored2 %>%
-  filter(score_1 %in% good_scores)%>%          #3523 records scored correctly by reviewer 1
-  filter(score_3 %in% good_scores)             #104 records scored correctly by both 1 AND 3
+  filter(score_1 %in% good_scores &
+           score_3 %in% good_scores &
+           !(score_2 %in% good_scores))%>%
+  rename(score_2_original = score_2,
+         score_2 = score_3) %>%
+  select(score_1, score_2, current_score, ID_Eeva) #n=7
 
+# papers scored correctly by only by reviewers 2 AND 3
 ok_scored23 <- scored2 %>%
-  filter(score_2 %in% good_scores)%>%          #3199 records scored correctly by reviewer 1
-  filter(score_3 %in% good_scores)             #102 records scored correctly by both 2 AND 3
+  filter(score_2 %in% good_scores &
+           score_3 %in% good_scores &
+           !(score_1 %in% good_scores))%>%
+  rename(score_1_original = score_1,
+         score_2_original = score_2,
+         score_1 = score_2,
+         score_2 = score_3)%>%
+  select(score_1, score_2, current_score, ID_Eeva)  #n=6
 
-# 
-a <- ok_scored13 %>% select(ID_Eeva) 
-b <- ok_scored23 %>% select(ID_Eeva)
-y <-
-  semi_join(a, b, by = "ID_Eeva") # 96 records scored by 3 reviewers, 8 records scored only by 2 and 3.
+# papers scored correctly by all three reviewers
+ok_scored123 <- scored2 %>%
+  filter(score_1 %in% good_scores &
+           score_2 %in% good_scores &
+           score_3 %in% good_scores)            #n=94
 
-#checking that reviewer 2 is different than reviewer 1
-a <- ok_scored12 %>% select(person_1) %>% rename('person' ="person_1")
-b <- ok_scored12 %>% select(person_2) %>% rename('person' ="person_2")
-y <-
-  semi_join(a, b, by = "person") #YES! always different persons
+#when 3 reviewers, select only two:
+#when all three agreed, select reviewers 1 & 2
+all_agree <- ok_scored123%>%
+  filter(score_1 == score_2 & 
+           score_1 == score_3)%>%
+  select(score_1, score_2, current_score, ID_Eeva) # n = 57
+
+#when reviewers 1 & 2 agree, but reviewer 3 disagree, select reviewers 1 & 3
+disagree1 <- ok_scored123 %>%
+  filter(score_1 ==  score_2 &
+           (score_3 != score_1))%>%
+  rename(score_2_original = score_2,
+         score_2 = score_3)%>%
+  select(score_1, score_2, current_score, ID_Eeva) #n = 15
+
+#when reviewers 1 & 2 disagree, select reviewers 1 & 2, except when the reviewer3 recommends 'include', then select reviewers 1 & 3
+disagree2 <- ok_scored123%>%
+  filter(score_1 !=  score_2 &
+           (score_3 != "include"))%>%
+  select(score_1, score_2, current_score, ID_Eeva)       #n = 16
+
+disagree3 <- ok_scored123%>%
+  filter(score_1 !=  score_2 &
+           (score_3 == "include"))%>%
+  rename(score_2_original = score_2,
+         score_2 = score_3)%>%
+  select(score_1, score_2, current_score, ID_Eeva)    #n = 6
+
+
+#combine all cases that were correctly reviewed by two reviewers into one dataframe
+scored3 <- bind_rows(ok_scored12, ok_scored13, ok_scored23,
+                     all_agree, disagree1, disagree2, disagree3) #n = 2704
+
 
 
 #Stats
 #-------------------------------
 
-# In how many percent of papers the two raters agreed on the score?
-# Note here, that agreeing on including is kind of heavier than agreeing on whether to exluclude based on title or abstract
-comp1 <- ok_scored12 %>% select(score_1,score_2)
-agree(comp1, tolerance=0) # 71.8% of the ratings are the same (out of three options)
+################
+#title screening
+################
+#how often did two reviewers agree on excluding a paper based on title?
 
-#"Limitation of percent agreement is that it does not take account of the possibility that raters guessed on scores. It thus may overestimate the true agreement among raters."
-#Cohen's kappa takes into account the possibility that two raters were not sure whether to include or exclude a paper and decided the score randomly:
-kap <- kappa2(comp1, weight = c("equal"), sort.levels = TRUE)
-kap # 0.441 -> weak agreement (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3900052/)
-#z = 34.3, p = 0
+title_screening <- scored3 %>%
+  mutate(score_1 = case_when(
+  .$score_1 == "exclude_abstract" ~ 'further',
+  .$score_1 == "exclude_title" ~ 'exclude_title',
+  .$score_1 == "include" ~ 'further'),
+  score_2 = case_when(
+  .$score_2 == "exclude_abstract" ~ 'further',
+  .$score_2 == "exclude_title" ~ 'exclude_title',
+  .$score_2 == "include" ~ 'further'))
 
+agree(title_screening[1:2], tolerance=0) # 78% of the cases reviewers agreed on exclude_title vs. further
+kap <- kappa2(title_screening[1:2], weight = c("equal"), sort.levels = TRUE)
+kap # Cohen's kappa = 0.541 -> moderate agreement
+#z = 28.2, p = 0, N = 2704
 
-#----
-#how often two reviewers agreed on including a paper?
-comp2 <- comp1 %>% mutate(score_1 = case_when(
-  .$score_1 == "exclude_abstract" ~ 'exclude',
-  .$score_1 == "exclude_title" ~ 'exclude',
-  .$score_1 == "include" ~ 'include'))
-comp2 <- comp2 %>% mutate(score_2 = case_when(
-  .$score_2 == "exclude_abstract" ~ 'exclude',
-  .$score_2 == "exclude_title" ~ 'exclude',
-  .$score_2 == "include" ~ 'include'))
-                                              
-agree(comp2, tolerance=0) # 87.9% of the cases reviewers agreed on include vs.exclude
-kap <- kappa2(comp2, weight = c("equal"), sort.levels = TRUE)
-kap # Cohen's kappa = 0.624 -> moderate agreement
-#z = 35.4, p = 0
+both_exclude_title <- title_screening %>% filter(score_1 == "exclude_title" &
+                                 score_2 == "exclude_title") #1332 out of 2704 = 49.3%
 
-both_incl <- comp1 %>% filter(score_1 == "include" &
-                                score_2 == "include")
-temp <- anti_join(comp1, both_incl, by = "score_1")#remove papers which both reviewers agreed to include
-comp3 <- temp %>% filter(score_2 != "include")#remove papers which reviewer 2 included
-#the reviewers agreed to include a paper in 394 cases out of 3174 checked, i.e. on 12.4 %
-#they agreed on excluding a paper in 2370 cases out of 3174 checked, i.e. on 74.7 %
-#they disagreed on whether to include/exclude in 3174-2370-394 = 410 cases, i.e. on 12.9%
+both_further <- title_screening %>% filter(score_1 == "further" &
+                                   score_2 == "further") # 778 out of 2704 = 28.8%
+# not agreeing on 582 cases out of 2704 = 22 %
 
 
-#----
-#if both reviewers excluded a paper, how often they did it based on the same argument (title vs. abstract)
-agree(comp3, tolerance=0) # in 78.2% of cases they agreed on why to exclude a paper (title vs. abstract)
-kap <- kappa2(comp3, weight = c("equal"), sort.levels = TRUE)
-kap # Cohen's kappa = 0.344 -> minimal agreement
-# z = 16.7, p = 0
 
-both_title <- comp3 %>% filter(score_1 == "exclude_title" &
-                                score_2 == "exclude_title")
+################
+#combine title & abstract screening
+################
+#how often did two reviewers agree on include vs. exclude a paper after BOTH title and abstract screening?
 
-#they agreed on excluding a paper based on title in 1596 cases out of 2348 checked, i.e. on 70.0%
+abstract_screening <- scored3 %>%
+  mutate(score_1 = case_when(
+    .$score_1 == "exclude_abstract" ~ 'exclude',
+    .$score_1 == "exclude_title" ~ 'exclude',
+    .$score_1 == "include" ~ 'include'),
+    score_2 = case_when(
+      .$score_2 == "exclude_abstract" ~ 'exclude',
+      .$score_2 == "exclude_title" ~ 'exclude',
+      .$score_2 == "include" ~ 'include'))
+
+agree(abstract_screening[1:2], tolerance=0) # 87.5% of the cases reviewers agreed on exclude vs. include
+kap <- kappa2(abstract_screening[1:2], weight = c("equal"), sort.levels = TRUE)
+kap # Cohen's kappa = 0.605 
+#z = 31.6, p = 0, N = 2704
+
+both_include <- abstract_screening %>% filter(score_1 == "include" &
+                                                score_2 == "include") #363 out of 2704 = 13.4%
+
+both_exclude <- abstract_screening %>% filter(score_1 == "exclude" &
+                                                score_2 == "exclude") #2003 out of 2704 = 74.1%
+# disagreeing in 333 cases out of 2704 = 12.5 %
+
+
+################
+#abstract screening
+################
+#how often did two reviewers agree on excluding a paper based on abstract screening?
+#filter first out papers that were excluded based on title
+abstract_screening <- scored3 %>%
+  filter(!(score_1 == "exclude_title" &
+             score_2 == "exclude_title"))%>%
+  mutate(score_1 = case_when(
+    .$score_1 == "exclude_abstract" ~ 'exclude',
+    .$score_1 == "exclude_title" ~ 'exclude',
+    .$score_1 == "include" ~ 'include'),
+    score_2 = case_when(
+      .$score_2 == "exclude_abstract" ~ 'exclude',
+      .$score_2 == "exclude_title" ~ 'exclude',
+      .$score_2 == "include" ~ 'include')) # n = 1372
+
+agree(abstract_screening[1:2], tolerance=0) # 75.7% of the cases reviewers agreed on exclude vs. include
+kap <- kappa2(abstract_screening[1:2], weight = c("equal"), sort.levels = TRUE)
+kap # Cohen's kappa = 0.49 
+#z = 18.3, p = 0, N = 1372
+
+both_include <- abstract_screening %>% filter(score_1 == "include" &
+                                                score_2 == "include") #368 out of 1372 = 26.8%
+
+both_exclude <- abstract_screening %>% filter(score_1 == "exclude" &
+                                                score_2 == "exclude") #670 out of 1372 = 48.8%
+# disagreeing in 334 cases out of 1324 = 24.3 %
