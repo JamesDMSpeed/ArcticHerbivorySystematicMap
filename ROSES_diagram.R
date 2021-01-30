01228b68344804971d26e4f6eff6c004fcf6a4d1
icbarrio <isabel@lbhi.is>
2021-01-30 19:03:15 +0000

    updating numbers to make sure the numbers are the same in the ROSES diagram and the evidence points in the app

--- ROSES_diagram.R
+++ ROSES_diagram.R
@@ -30,7 +30,7 @@ library(tidyverse)
 #instead, I just saved the file in my computer and import from there
 
 #set the working directory
-setwd("C:/Users/isabel/OneDrive - Landbúnaðarháskóli Íslands/ISABEL/HN systematic map")
+setwd("C:/Users/isabel/OneDrive - Landbúnaðarháskóli Íslands/ISABEL/HERBIVORY NETWORK/HN systematic map")
 overview <- read_excel("Overview_all_scored.xlsx", sheet="all entries", range=cell_cols("A:AD"))
 
 #check
@@ -56,17 +56,17 @@ score[score$current_score == "remove_duplicate", ]$n  #684
   length(levels(as.factor(overview$ID_Eeva))) - score[score$current_score == "remove_duplicate", ]$n  #2902
 
 #number of documents excluded at the title stage            
-score[score$current_score == "exclude_title", ]$n  #1334
+score[score$current_score == "exclude_title", ]$n  #1332
   #number of documents after title screening
   length(levels(as.factor(overview$ID_Eeva))) - score[score$current_score == "remove_duplicate", ]$n -
-                                                score[score$current_score == "exclude_title", ]$n  #1568
+                                                score[score$current_score == "exclude_title", ]$n  #1570
 
 #number of documents excluded at the abstract stage            
-score[score$current_score == "exclude_abstract", ]$n  #667
+score[score$current_score == "exclude_abstract", ]$n  #670
   #number of documents after title screening
   length(levels(as.factor(overview$ID_Eeva))) - score[score$current_score == "remove_duplicate", ]$n -
                                                 score[score$current_score == "exclude_title", ]$n -
-                                                score[score$current_score == "exclude_abstract", ]$n  #901
+                                                score[score$current_score == "exclude_abstract", ]$n  #900
 
 #number of unretrievable full texts            
 score[score$current_score == "exclude_no_full_text", ]$n  #44
@@ -74,22 +74,22 @@ score[score$current_score == "exclude_no_full_text", ]$n  #44
   length(levels(as.factor(overview$ID_Eeva))) - score[score$current_score == "remove_duplicate", ]$n -
                                                 score[score$current_score == "exclude_title", ]$n -
                                                 score[score$current_score == "exclude_abstract", ]$n -
-                                                score[score$current_score == "exclude_no_full_text", ]$n  #857
+                                                score[score$current_score == "exclude_no_full_text", ]$n  #856
 
 #there are several categories that refer to unsuitable document types:
 #correction to published articles, raw datasets, maps and supplementary materials
 sum(score[score$current_score %in% c("remove_correction","remove_dataset",
-                                 "remove_map","remove_suppplement"), ]$n)  #16
+                                 "remove_map","remove_suppplement"), ]$n)  #15
 
 #studies excluded based on locality (both exclude_site_coordinates and exclude_site_text)
-sum(score[score$current_score %in% c("exclude_site_coordinates","exclude_site_text"), ]$n)  #93
+sum(score[score$current_score %in% c("exclude_site_coordinates","exclude_site_text"), ]$n)  #88
 
 
 #total number of studies excluded at full stage phase
 sum(score[score$current_score %in% c("remove_correction","remove_dataset",
-                                 "remove_map","remove_suppplement", 
+                                 "remove_map","remove_suppplement", "exclude_redundant",
                                  "exclude_site_coordinates","exclude_site_text",
-                                 "exclude_full_text"), ]$n)  #533
+                                 "exclude_full_text"), ]$n)  #536
 
 #number of documents after full text screening
   length(levels(as.factor(overview$ID_Eeva))) - score[score$current_score == "remove_duplicate", ]$n -
@@ -97,20 +97,21 @@ sum(score[score$current_score %in% c("remove_correction","remove_dataset",
                                                 score[score$current_score == "exclude_abstract", ]$n -
                                                 score[score$current_score == "exclude_no_full_text", ]$n -
                                                 sum(score[score$current_score %in% c("remove_correction","remove_dataset",
-                                                       "remove_map","remove_suppplement", 
+                                                       "remove_map","remove_suppplement", "exclude_redundant", 
                                                        "exclude_site_coordinates","exclude_site_text",
-                                                       "exclude_full_text"), ]$n)  #324
+                                                       "exclude_full_text"), ]$n)  #320
   #this should be the same as 
     score[score$current_score == "include", ]$n
     
 #reasons for excluding at full text stage
+score[score$current_score == "exclude_redundant", ]$n #8
 full_text_excl <- overview %>% filter(current_score == "exclude_full_text") %>% 
                       group_by(full_text_exclusion) %>% 
                       count()
 full_text_excl[full_text_excl$full_text_exclusion == "no_population", ]$n  #55
-full_text_excl[full_text_excl$full_text_exclusion == "no_exposure", ]$n  #22
+full_text_excl[full_text_excl$full_text_exclusion == "no_exposure", ]$n  #24
 full_text_excl[full_text_excl$full_text_exclusion == "no_comparator", ]$n  #11
-sum(full_text_excl[full_text_excl$full_text_exclusion %in% c("no_outcome","no_outcome_data"), ]$n)  #316
+sum(full_text_excl[full_text_excl$full_text_exclusion %in% c("no_outcome","no_outcome_data"), ]$n)  #315
   #this category includes studies that did not measure an effect of herbivores on plants
   #and studies that did not include primary data (e.g. synthesis papers, reviews, conceptual papers)
 full_text_excl[full_text_excl$full_text_exclusion == "no_study_design", ]$n  #20
@@ -136,15 +137,11 @@ theses %>%
 
 ##compare dataset with separate evidence points
 #from James' script putting all data together
-alldata_evid <- alldata %>% separate(evidence_point_ID,c("Eeva_ID", "evid_ID"),sep="_")
+AllCodedDataW <- read.csv("C:/ISABEL/R/ArcticHerbivorySystematicMap/Data/AllCodedDataW.txt", sep=";")
 
-#not sure why these two are different?
-length(levels(as.factor(alldata_evid$Eeva_ID)))#328
-length(levels(as.factor(alldata_evid$title)))#329
-
-full_text_incl <- overview %>% filter(current_score == "include") 
-
-
-setdiff(full_text_incl$ID_Eeva,alldata_evid$Eeva_ID)
+#Number of studies
+length(levels(as.factor(AllCodedDataW$title)))  #315
+#Number of evidence points
+length(levels(as.factor(AllCodedDataW$evidence_point_ID)))  #678
 
 
